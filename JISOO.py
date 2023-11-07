e903a523700cd0a845c795bc6e29a1ab46f69f1e
SargsyanVrej <vrej.sargsyan777@mail.ru>
2023-11-07 20:05:26 +0400

    python

---
+++ JISOO.py
@@ -0,0 +1,37 @@
+# solo - jennie
+# on the ground - rose
+# gone - rose
+# lalisa - lisa
+# money - lisa
+# hard to love - rose
+# flower - jisoo
+# all eyes on me - jisoo
+# you & me - jennie
+
+
+# mylist = [180, -1, 160, 170, -1, -1, -1, 150, 190, -1, 110]
+# sorted_values = sorted([x for x in mylist if x != -1])
+# result = (x if x != -1 else sorted_values.pop(0) for x in mylist)
+# result = list(result)
+
+# print(result)
+
+year = int(input('>>>'))
+
+# if year % 4 == 0 and year % 100 != 0 and year % 400 != 0:
+#     print(f'{year}year is leap.')
+# else:
+#     print(f'{year} year is no leap')
+
+# if year % 400 == 0:
+#     if year % 100 == 0: 
+#         if year % 4 == 0:
+#             print('Leap year')
+#         else:
+#             print('Not leap')
+# else:
+#     print('Not leap')
+
+
+    
+    
